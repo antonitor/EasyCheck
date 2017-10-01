d079020e136e36d5f54c22d81b80ed501e3fbb2b
Toni <tonietorres@gmail.com>
2017-10-01 20:39:39 +0200

    Afegit métode gestor per defecte a preferences

--- app/src/main/java/net/marcarni/easycheck/MainActivity.java
+++ app/src/main/java/net/marcarni/easycheck/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ((Button)findViewById(R.id.scan)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
