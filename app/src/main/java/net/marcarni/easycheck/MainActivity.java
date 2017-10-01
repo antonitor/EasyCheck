dda3f4e62dfc166cdd6907d07fd316dccc6b751e
Toni <tonietorres@gmail.com>
2017-10-01 20:57:32 +0200

    Afegides condicions per accedir al mètode gestor per defecte, de moment tan sols mostra el mètode a un textview

--- app/src/main/java/net/marcarni/easycheck/MainActivity.java
+++ app/src/main/java/net/marcarni/easycheck/MainActivity.java
@@ -1,10 +1,13 @@
 package net.marcarni.easycheck;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        TextView defaultMethodTextView = findViewById(R.id.defecte);
+        String defaultMethod = sharedPreferences.getString(getString(R.string.pref_manager_default_key), "");
+        if (defaultMethod.equals(getString(R.string.pref_manager_default_qr_value)) ) {
+            defaultMethodTextView.setText(getString(R.string.pref_manager_default_qr_label));
+        } else if (defaultMethod.equals(getString(R.string.pref_manager_default_dni_value))) {
+            defaultMethodTextView.setText(getString(R.string.pref_manager_default_dni_label));
+        } else {
+            defaultMethodTextView.setText(getString(R.string.pref_manager_default_loc_label));
+        }
 
 
         ((Button)findViewById(R.id.scan)).setOnClickListener(new View.OnClickListener() {
