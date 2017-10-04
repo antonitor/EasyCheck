1b02030ffa253eae8d0da2ac3d0b731e6bb7c504
CarlosCastro2 <carloscastrocanabate@gmail.com>
2017-10-05 01:40:54 +0200

    Missatge confirmar Check-in + Color

--- app/src/main/java/net/marcarni/easycheck/RecyclerView/HeaderAdapter.java
+++ app/src/main/java/net/marcarni/easycheck/RecyclerView/HeaderAdapter.java
@@ -1,13 +1,17 @@
 package net.marcarni.easycheck.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import net.marcarni.easycheck.R;import java.util.ArrayList;
+import net.marcarni.easycheck.R;
 
+import java.util.ArrayList;
 
 
 public class HeaderAdapter extends RecyclerView.Adapter<HeaderAdapter.ViewHolder> {
@@ -16,21 +20,47 @@ public class HeaderAdapter extends RecyclerView.Adapter<HeaderAdapter.ViewHolder
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-     static class ViewHolder extends RecyclerView.ViewHolder {
+
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         // each data item is just a string in this case
         TextView headerCode;
-         TextView responsableName;
-        TextView dni,data;
-
-
+        TextView responsableName;
+        TextView dni, data;
+        View v;
         ViewHolder(View v) {
             super(v);
-            headerCode= (TextView)v.findViewById(R.id.headerCode);
-            responsableName=(TextView) v.findViewById(R.id.headerDetall);
-            dni=(TextView)v.findViewById(R.id.dni);
-            data=(TextView)v.findViewById(R.id.data);
+            headerCode = (TextView) v.findViewById(R.id.headerCode);
+            responsableName = (TextView) v.findViewById(R.id.headerDetall);
+            dni = (TextView) v.findViewById(R.id.dni);
+            data = (TextView) v.findViewById(R.id.data);
+            v.setOnClickListener(this);
 
+        }
 
+        @Override
+        public void onClick(View view) {
+            v = view;
+            AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+            builder.setMessage("Vols Confirmar el Check-IN?")
+                    .setTitle("Atenció!!")
+                    .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int i) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setPositiveButton("Acceptar",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                    v.setBackgroundColor(Color.GREEN);
+                                    //v.setTag(0,"CHECK");
+                                    //String tag = (String) v.getTag ();
+                                }
+                            }
+                    );
+            AlertDialog alert = builder.create();
+            alert.show();
         }
     }
 
@@ -48,6 +78,7 @@ public class HeaderAdapter extends RecyclerView.Adapter<HeaderAdapter.ViewHolder
                 .inflate(R.layout.item_header, parent, false);
         // set the view's size, margins, paddings and layout parameters
 
+
         return  new ViewHolder(v);
 
     }
@@ -64,6 +95,8 @@ public class HeaderAdapter extends RecyclerView.Adapter<HeaderAdapter.ViewHolder
 
 
 
+
+
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -71,5 +104,5 @@ public class HeaderAdapter extends RecyclerView.Adapter<HeaderAdapter.ViewHolder
     public int getItemCount() {
         return mDataset.size();
     }
-}
 
+}
