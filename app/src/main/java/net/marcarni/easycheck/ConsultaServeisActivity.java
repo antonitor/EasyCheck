385d0a88f32925cfb105c9516962855b3f726040
Toni <tonietorres@gmail.com>
2017-10-07 18:31:20 +0200

    Afegida funcionalitat del botó per escollir data al toolbar i comentaris TODO per aclarir el funcionament de la classe

--- app/src/main/java/net/marcarni/easycheck/ConsultaServeisActivity.java
+++ app/src/main/java/net/marcarni/easycheck/ConsultaServeisActivity.java
@@ -1,10 +1,13 @@
 package net.marcarni.easycheck;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
@@ -13,6 +16,8 @@ import android.widget.Toast;
 import net.marcarni.easycheck.settings.MenuAppCompatActivity;
 
 public class ConsultaServeisActivity extends MenuAppCompatActivity {
+
+    private static final int DATE_PICKER_REQUEST = 22;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +29,8 @@ public class ConsultaServeisActivity extends MenuAppCompatActivity {
         editToolbar.inflateMenu(R.menu.toolbar_menu);
         Spinner spinnerTreballadors = (Spinner) findViewById(R.id.spinner_de_treballadors);
 
-        //Cursor amb dades test, s'ha d'esborrar el métode getFakeCursor y extraure-les de la bbdd
-        Cursor cursorTest =  getFakeCursor(); //db.obtenirLlistaDeTreballadors();
+        //TODO 1: Cursor amb dades test, s'ha d'esborrar el métode getFakeCursor y extraure-les de la bbdd
+        Cursor cursorTest =  getFakeCursor(); //--->>> db.obtenirLlistaDeTreballadors();
 
         android.widget.SimpleCursorAdapter  adapter = new android.widget.SimpleCursorAdapter(this,
                 android.R.layout.simple_spinner_dropdown_item,
@@ -35,13 +40,44 @@ public class ConsultaServeisActivity extends MenuAppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTreballadors.setAdapter(adapter);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ((MenuItem)findViewById(R.id.seleccionar_data)).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent intent = new Intent (ConsultaServeisActivity.this,CalendarActivity.class);
+                startActivityForResult(intent, DATE_PICKER_REQUEST);
+                return true;
+            }
+        });
         if (spinnerTreballadors != null) {
             spinnerTreballadors.setAdapter(adapter);
-
             spinnerTreballadors.setOnItemSelectedListener(new myOnItemSelectedListener());
         }
     }
 
+    /**
+     * Recull el resultat de CalendarActivity
+     * @param requestCode
+     * @param resultCode
+     * @param intent
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(requestCode == DATE_PICKER_REQUEST)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                //TODO 2: Recollir la data seleccionada a l'activitat Calendar i actualitzar el recycler aquí
+                String data = intent.getStringExtra("DATA");
+            }
+        }
+    }
+
+    /*
+     * //TODO 4: Aquest mètode es pot esborrar un cop extreiem els treballadors de la bbdd sqlite
+     *
+     * @return
+     */
     public MatrixCursor getFakeCursor(){
         String[] columns = new String[] { "_id", "treballador" };
 
@@ -55,11 +91,14 @@ public class ConsultaServeisActivity extends MenuAppCompatActivity {
     }
 }
 
+/**
+ * Listener per els items seleccionats al Spinner amb nombs de treballador
+ */
 class myOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-        //Recarregar aquí el RecyclerView segons el treballador seleccionat
+        //TODO 3: Recarregar aquí el RecyclerView segons el treballador seleccionat (el paràmetre id correspón a la columna _id de treballadors)
         Toast.makeText(view.getContext(), "Treballador amb _ID = " + id + " seleccionat.", Toast.LENGTH_SHORT ).show();
     }
 
