18c57b1ea14010377dad171653fbd55ec5b2d971
Maria <remediosortegacobo@gmail.com>
2017-10-10 18:57:03 +0200

    consulta treballador x data x dia (sense probar)

--- app/src/main/java/net/marcarni/easycheck/SQLite/DBInterface.java
+++ app/src/main/java/net/marcarni/easycheck/SQLite/DBInterface.java
@@ -290,6 +290,21 @@ public class DBInterface {
         return bd.rawQuery(consultaSQL, args);
 
     }
+    public Cursor RetornaServei_Treballador_data_hora(int id,String data,String hora) {
+
+        String[] args = new String[]{String.valueOf(id), data,hora};
+
+        String consultaSQL = "Select distinct t." + Treballador.NOM + ", t." + Treballador.COGNOM1 + ", t." + Treballador.COGNOM2 +
+                ", s." + Serveis.DESCRIPCIO + ", r." + Reserves.ID_SERVEI + ", s." + Serveis.DATA_SERVEI + ", s." + Serveis.HORA_INICI + ", s." + Serveis.HORA_FI +
+                " FROM " + Serveis.NOM_TAULA + " s " +
+                " JOIN " + Treballador.NOM_TAULA + " t ON t." + Treballador._ID + " = s." + Serveis.ID_TREBALLADOR +
+                " and t." + Treballador._ID + "= ? and s." + Serveis.DATA_SERVEI + " = ? and s."+Serveis.HORA_INICI+" = ? " +
+                " LEFT join " + Reserves.NOM_TAULA + " r  on s." + Serveis._ID + " = r." + Reserves.ID_SERVEI + "";
+
+
+        return bd.rawQuery(consultaSQL, args);
+
+    }
     public Cursor RetornaServei_data(String data) {
 
         String[] args = new String[]{data};
