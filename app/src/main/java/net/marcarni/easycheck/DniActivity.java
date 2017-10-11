f7d4cfdd1ef130204b641669f336d2c41e539914
Maria <remediosortegacobo@gmail.com>
2017-10-11 21:26:06 +0200

    Calendario convertido en Picker

--- app/src/main/java/net/marcarni/easycheck/DniActivity.java
+++ app/src/main/java/net/marcarni/easycheck/DniActivity.java
@@ -1,9 +1,13 @@
 package net.marcarni.easycheck;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -11,6 +15,8 @@ import android.widget.Toast;
 
 import net.marcarni.easycheck.eines.ValidaDNI;
 import net.marcarni.easycheck.settings.MenuAppCompatActivity;
+
+import java.util.Calendar;
 
 public class DniActivity extends MenuAppCompatActivity {
     static final int ACTIVITAT_DATA = 1;
@@ -22,6 +28,8 @@ public class DniActivity extends MenuAppCompatActivity {
     ImageButton buttonData;
     String cadena = null;
     ValidaDNI validaDni = new ValidaDNI();
+    String data_="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,14 +86,47 @@ public class DniActivity extends MenuAppCompatActivity {
             }
         });
         buttonData.setOnClickListener(new View.OnClickListener(){
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////
+            //////////////              PICKER AÑADIDO      //////////////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////
+            
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (DniActivity.this,CalendarActivity.class);
-                startActivityForResult(intent, 1); // L'activitat retornarà un resultat.
+
+                Calendar mcurrentDate = Calendar.getInstance();
+                int mYear = mcurrentDate.get(Calendar.YEAR);
+                int mMonth = mcurrentDate.get(Calendar.MONTH);
+                int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker;
+                mDatePicker = new DatePickerDialog(DniActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                        // TODO Auto-generated method stub
+                    /*      Your code   to get date and time    */
+                        selectedmonth = selectedmonth + 1;
+                       data_="" + selectedday + "/" + selectedmonth + "/" + selectedyear;
+                        textViewData.setText(data_);
+                    }
+                }, mYear, mMonth, mDay);
+                mDatePicker.setTitle("Select Date");
+                mDatePicker.show();
             }
         });
+
+
+
+
+
+
+
+               /* Intent intent = new Intent (DniActivity.this,CalendarActivity.class);
+                startActivityForResult(intent, 1); // L'activitat retornarà un resultat.*/
+
     }
-    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+   /* public void onActivityResult(int requestCode, int resultCode, Intent intent)
     {
         //Comprovem a quina activitat correspon
         if(requestCode == ACTIVITAT_DATA)
@@ -98,7 +139,7 @@ public class DniActivity extends MenuAppCompatActivity {
                 textViewData.setText(data);
             }
         }
-    }
+    }*/
 
 
 
